25e4cf3b69ca7dd4ffc89aa9c12cce800970c01b
Maja Bevanda <maja.bevanda@hotmail.com>
2020-09-30 06:52:22 +0200

    Adding data design.
    Braking out the data that needs to be stored or used in application into data models.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        /// <summary>
+        /// List of members in one team
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        /// <summary>
+        /// Name of team.
+        /// </summary>
+        public string TeamName { get; set; }
+    }
+}
