231580ba7404cf7ba518b5266e6c6c1079055062
Maja Bevanda <maja.bevanda@hotmail.com>
2020-12-17 16:29:26 +0100

    Tournament viewer, populated rouns dropdown

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -9,21 +9,48 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TrackerLibrary.Models;
 
+
 namespace TrackerUI
 {
     public partial class TournamentViewerForm : Form
     {
         private TournamentModel tournament;
+        List<int> rounds = new List<int>();
         public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
             tournament = tournamentModel;
             LoadFormData();
+            LoadRounds();
         }
 
         private void LoadFormData()
         {
             tournamentName.Text = tournament.TournamentName;
+        }
+
+        private void WireUpLists()
+        {
+            roundDropDown.DataSource = null;
+            roundDropDown.DataSource = rounds;
+        }
+
+        private void LoadRounds()
+        {
+            rounds = new List<int>();
+
+            rounds.Add(1);
+            int currRound = 1;
+
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                if (matchups.First().MatchupRound > currRound)
+                {
+                    currRound = matchups.First().MatchupRound;
+                    rounds.Add(currRound);
+                }
+            }
+            WireUpLists();
         }
     }
 }
