60df13a47122d7d684c65c38cc33176f1f3a4433
Maja Bevanda <maja.bevanda@hotmail.com>
2021-01-05 14:16:35 +0100

    Added feature for emailing users

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -119,7 +119,9 @@ namespace TrackerUI
 
             TournamentLogic.CreateRounds(tm);
 
-            GlobalConfig.Connection.CreateTournament(tm);            
+            GlobalConfig.Connection.CreateTournament(tm);  
+            
+            tm.AlertUsersToNewRound();
 
             TournamentViewerForm frm = new TournamentViewerForm(tm);
             frm.Show();
